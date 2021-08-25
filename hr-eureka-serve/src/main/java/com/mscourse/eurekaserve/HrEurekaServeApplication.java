ecd9dcbdb3bd01ca4adccb5070ea193d7bcecbea
FabioBenjamim <fabenjamim15@gmail.com>
2021-08-25 19:33:55 -0300

    config Eureka server

--- hr-eureka-serve/src/main/java/com/mscourse/eurekaserve/HrEurekaServeApplication.java
+++ hr-eureka-serve/src/main/java/com/mscourse/eurekaserve/HrEurekaServeApplication.java
@@ -2,7 +2,9 @@ package com.mscourse.eurekaserve;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class HrEurekaServeApplication {
 
