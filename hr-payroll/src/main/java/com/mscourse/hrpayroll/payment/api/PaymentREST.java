6e87174e35b2f47c51360b386ce7e2166814bab8
FabioBenjamim <fabenjamim15@gmail.com>
2021-08-26 17:21:37 -0300

    create and config gateway-zuul

--- hr-payroll/src/main/java/com/mscourse/hrpayroll/payment/api/PaymentREST.java
+++ hr-payroll/src/main/java/com/mscourse/hrpayroll/payment/api/PaymentREST.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.mscourse.hrpayroll.payment.Payment;
 import com.mscourse.hrpayroll.payment.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 @RequestMapping(value = "/payment")
@@ -17,8 +18,13 @@ public class PaymentREST {
 	@Autowired
 	PaymentService service = new PaymentService();
 	
+	@HystrixCommand(fallbackMethod = "getPaymentAlternative")
 	@GetMapping(value = "/{workerId}/days/{days}")
 	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days){
 		return ResponseEntity.ok(service.getPayment(workerId, days));
+	}
+	
+	public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days){
+		return (ResponseEntity<Payment>) ResponseEntity.badRequest();
 	}
 }
