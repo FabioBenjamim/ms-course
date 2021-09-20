8bc01b2316a820f94cbac5e08739ee0b3b35c740
FabioBenjamim <fabenjamim15@gmail.com>
2021-09-20 20:57:14 -0300

    hr-worker config server client

--- hr-worker/src/main/java/com/mscourse/hrworker/worker/api/WorkerREST.java
+++ hr-worker/src/main/java/com/mscourse/hrworker/worker/api/WorkerREST.java
@@ -3,11 +3,10 @@ package com.mscourse.hrworker.worker.api;
 import java.util.List;
 import java.util.Optional;
 
-import javax.websocket.server.PathParam;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,6 +24,9 @@ public class WorkerREST {
 	
 	private static Logger logger = LoggerFactory.getLogger(WorkerREST.class);
 	
+	@Value("${test.config}")
+	private String testConfig;
+	
 	@Autowired
 	private Environment env;
 
@@ -34,6 +36,12 @@ public class WorkerREST {
 	@GetMapping
 	public ResponseEntity<List<Worker>> findAll(){
 		return ResponseEntity.ok(repository.findAll());
+	}
+	
+	@GetMapping(value = "/configs")
+	public ResponseEntity<Void> getConfig(){
+		logger.info("Config: " + testConfig);
+		return ResponseEntity.noContent().build();
 	}
 	
 	@GetMapping(value = "/{id}")
